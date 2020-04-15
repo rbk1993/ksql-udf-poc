211f280e4f0bd67ea550c990dde84dd8e5bae398
Reda BENKIRANE <reda.benkirane@chanel.com>
2020-04-15 10:43:59 +0200

    fix UdfParameters

--- src/main/java/chanel/pioneer/ksql/udfdemo/ArrayOfStructContainsRegion.java
+++ src/main/java/chanel/pioneer/ksql/udfdemo/ArrayOfStructContainsRegion.java
@@ -17,8 +17,8 @@ public class ArrayOfStructContainsRegion {
 
 	  @Udf(description = "checks if an array of struct contains a region")
 	  public <T> Boolean containsRegion(
-			  @UdfParameter(schema = "ARRAY<STRUCT<REGIONID VARCHAR, DIVISIONID VARCHAR, MARKETID VARCHAR>>") final List<Struct> markets,
-			  @UdfParameter final String region
+			  @UdfParameter(value="markets", schema = "ARRAY<STRUCT<REGIONID VARCHAR, DIVISIONID VARCHAR, MARKETID VARCHAR>>") final List<Struct> markets,
+			  @UdfParameter("region") final String region
 	  ) {
 		  for(int i=0;markets != null && i<markets.size();i++) {
 			  System.out.println("markets["+i+"]->"+REGION_ID+" = "+markets.get(i).getString(REGION_ID));
